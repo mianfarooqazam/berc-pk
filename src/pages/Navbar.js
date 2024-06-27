50e6289fee9ecf53e923fe0482fc86243164eb7b
Farooq <farooqazammian@gmail.com>
2024-06-27 12:48:08 +0500

    UI Updated

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -3,29 +3,30 @@ import logo from '../assets/berc-logo.jpeg';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-10">
+    <nav className="fixed top-0 left-0 w-full shadow-md z-10" style={{ backgroundColor: '#fafafa' }}>
       <div className="flex items-center justify-between p-4">
         <div className="flex items-center space-x-4">
-          <img src={logo} alt="Logo" className="h-10" />
+          <img src={logo} alt="Logo" className="h-14" />
         </div>
         <ul className="flex space-x-8">
           <li>
-            <a href="#home" className="text-lg font-semibold text-gray-700">Home</a>
+            <a href="#home" className="text-lg font-medium text-gray-700">Home</a>
           </li>
           <li>
-            <a href="#why-us" className="text-lg font-semibold text-gray-700">Why Us</a>
+            <a href="#why-us" className="text-lg font-medium text-gray-700">Why Us</a>
           </li>
           <li>
-            <a href="#tools" className="text-lg font-semibold text-gray-700">Tools</a>
+            <a href="#services" className="text-lg font-medium text-gray-700">Services</a>
           </li>
           <li>
-            <a href="#services" className="text-lg font-semibold text-gray-700">Services</a>
+            <a href="#tools" className="text-lg font-medium text-gray-700">Tools</a>
           </li>
           <li>
-            <a href="#team" className="text-lg font-semibold text-gray-700">Team</a>
+            <a href="#team" className="text-lg font-medium text-gray-700">Team</a>
           </li>
+          
           <li>
-            <a href="#lets-talk" className="text-lg font-semibold text-gray-700">Let's Talk</a>
+            <a href="#lets-talk" className="text-lg font-medium text-gray-700">Let's Talk</a>
           </li>
         </ul>
       </div>
