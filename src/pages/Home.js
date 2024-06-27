84cd515a151fac1094b7e21f7b79f2942a6c7d06
Farooq <farooqazammian@gmail.com>
2024-06-27 10:15:07 +0500

    UI Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,24 @@
-
-
 import React from 'react';
 import AnimatedText from '../components/AnimatedText';
-import bg from '../assets/City-1.jpg'; 
+import bg from '../assets/b6.jpg'; 
 
 const Home = () => {
   return (
-    <section id="home" className="relative h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${bg})` }}>
-      <div className="absolute inset-0 bg-black opacity-50"></div> {/* Overlay for better text visibility */}
-      <AnimatedText />
+    <section id="home" className="relative h-screen flex">
+      <div 
+        className="w-2/5 h-full flex items-center justify-center" 
+        style={{ 
+          background: 'linear-gradient(180deg, hsla(190, 34%, 57%, 1) 0%, hsla(187, 48%, 77%, 1) 100%)'
+        }}
+      >
+        <AnimatedText />
+      </div>
+      <div 
+        className="w-3/5 h-full bg-cover bg-center" 
+        style={{ 
+          backgroundImage: `url(${bg})`
+        }}
+      ></div>
     </section>
   );
 };
