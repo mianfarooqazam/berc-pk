b4e687a39832cbb0dcdcf340af4e7af7447ee15f
Farooq <farooqazammian@gmail.com>
2024-07-01 15:23:59 +0500

    UI Improved

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Avatar } from '@mui/material';
 import Slide from '@mui/material/Slide';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const Modal = ({ open, handleClose, title, description }) => {
+const Modal = ({ open, handleClose, title, description, img }) => {
   return (
     <Dialog
       open={open}
@@ -16,11 +16,18 @@ const Modal = ({ open, handleClose, title, description }) => {
       aria-labelledby="alert-dialog-slide-title"
       aria-describedby="alert-dialog-slide-description"
     >
-      <DialogTitle id="alert-dialog-slide-title">{title}</DialogTitle>
+      <DialogTitle id="alert-dialog-slide-title" style={{fontWeight: "bold"}}>{title}</DialogTitle>
       <DialogContent>
-        <p id="alert-dialog-slide-description">
-          {description}
-        </p>
+        <div className="flex flex-col items-center">
+          <Avatar 
+            src={img} 
+            alt={title} 
+            sx={{ width: 96, height: 96, mb: 4 }}
+          />
+          <p id="alert-dialog-slide-description">
+            {description}
+          </p>
+        </div>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -77,7 +77,7 @@ const Team = () => {
                     sx={{ width: 96, height: 96, mb: 4 }}
                   />
                   <div>
-                    <h3 className="text-xl font-semibold mb-2" >{member.name}</h3>
+                    <h3 className="text-xl font-semibold mb-2 text-center">{member.name}</h3>
                     <p className="text-gray-600">{member.role}</p>
                   </div>
                   <div className="absolute bottom-0 left-0 w-full text-center pb-2 hidden group-hover:block">
@@ -95,6 +95,7 @@ const Team = () => {
         handleClose={handleClose}
         title={selectedMember.name}
         description={selectedMember.description}
+        img={selectedMember.img} 
       />
     </div>
   );
